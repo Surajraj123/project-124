39256142f90d434aa7d6f9ca2e09b527fffa0f85
Surajraj123 <121552259+Surajraj123@users.noreply.github.com>
2023-08-07 16:50:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    tint_color = "";
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+
+function draw()
+{
+    image(video, 0, 0, 400, 400);
+    tint(tint_color);
+}
+
+function take_snapshot()
+{
+    save("Suraj.png");
+}
+
+function filter_tint()
+{
+    tint_color = document.getElementById("color_name").value
+}
